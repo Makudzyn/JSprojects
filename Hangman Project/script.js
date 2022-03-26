528ccdadd220946fab40e6166a395597a4f2edd8
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2022-03-26 10:25:21 +0200

    added a few fixes
    added win and lose conditions, fixed entered letters repeat in array and some minor changes

--- Hangman Project/script.js
+++ Hangman Project/script.js
@@ -1,9 +1,11 @@
 let guessingWords = ["javascript", "apple", "notebook", "wolf", "phone", "monster"];
-let secretWord = [], hp = 8;
+let secretWord = [], hp = 8, rightAnswer = false, letterUsed = false;
 let guessWord = guessingWords[Math.floor(Math.random() * guessingWords.length)];
 for (let i = 0; i < guessWord.length; i++){
     secretWord[i] = "_";
 }
+
+document.getElementById("hpElement").innerText = hp;
 
 let tries = 0, enteredLetters = [];
 
@@ -11,43 +13,77 @@ let minutes = 0, seconds = 0, interval;
 const minutesCount = document.getElementById("minutesElement");
 const secondsCount = document.getElementById("secondsElement");
 
-document.getElementById("secretWordArea").innerHTML = secretWord.join(" ");
+document.getElementById("secretWordArea").innerText = secretWord.join(" ");
 
 document.getElementById("enterLetter").addEventListener("click", function () {
    let usersLetter = document.getElementById("enterSpaceArea").value;
     document.getElementById("enterSpaceArea").value = "";
+
    if (usersLetter === ""){
        printInfo("Вы ничего не ввели!");
+       return;
    }
    if (usersLetter.length > 1){
        printInfo("Вы ввели больше одной буквы! Если хотите ввести слово целиком нажмите другую кнопку.");
+       return;
    }
     if (/[^A-zА-яЁё]/.test(usersLetter)){
         printInfo("Нельзя вводить цифры и символы!");
+        return;
     }
-    if (tries === 0){
-        enteredLetters.push(usersLetter);
-    }else if(tries !== 0){
-        for (let i = 0; i < enteredLetters.length; i++){
+
+    if(tries !== 0){
+        for (let i = 0; i <= enteredLetters.length; i++){
             if (usersLetter === enteredLetters[i]){
                 printInfo("Вы уже вводили такую букву!");
-            }else if(usersLetter !== enteredLetters[i]){
-                enteredLetters.push(usersLetter);
+                letterUsed = true;
+                return;
             }
         }
+        if(letterUsed === false){
+            enteredLetters.push(usersLetter);
+        }
+
+    }else if (tries === 0) {
+        enteredLetters.push(usersLetter);
     }
 
     if (tries === 0) {
         clearInterval(interval);
         interval = setInterval(timerStart, 1000);
     }
-
     tries++;
+
+
+    for (let i = 0; i < guessWord.length; i++){
+        if(guessWord[i] === usersLetter){
+            printInfo("Верно! Вы отгадали букву.");
+            secretWord[i] = usersLetter;
+            document.getElementById("secretWordArea").innerText = secretWord.join(" ");
+            rightAnswer = true;
+            if (guessWord === secretWord.join("")){
+                printInfo("Поздравляю, вы отгадали слово!!!");
+                clearInterval(interval);
+            }
+        }else if(i === guessWord.length - 1 && rightAnswer === false){
+            printInfo("Такой буквы нет в загаданном слове! Попробуйте снова.");
+            hp--;
+            document.getElementById("hpElement").innerText = hp;
+            if (hp === 0){
+                printInfo("Попытки кончились, вы не смогли отгадать слово...");
+                clearInterval(interval);
+            }
+            return;
+        }
+    }
+
+    letterUsed = false;
+    rightAnswer = false;
+
 });
 
 function printInfo(str){
     document.getElementById("gameInfo").innerText = str;
-    return;
 }
 
 function timerStart(){
@@ -71,7 +107,7 @@ function timerStart(){
     if(minutes > 9){
         minutesCount.innerText = minutes;
     }
-    if(seconds > 59){
+    if(minutes > 59){
         printInfo("Ну и ну! Потратить час на отгадывание слова, никуда не годится, вы проиграли!");
     }
 }
