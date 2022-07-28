0f8f94e2dae3b03059f900521680efac2d0ee7a7
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2022-07-28 17:15:00 +0300

    simple piano from hyperskill courses

---
+++ VirtualPiano/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyA") {
+        let soundA = new Audio("SoundKeys/A.mp3");
+        soundA.play();
+    } else if(event.code === "KeyS"){
+        let soundS = new Audio("SoundKeys/S.mp3");
+        soundS.play();
+    } else if(event.code === "KeyD"){
+        let soundD = new Audio("SoundKeys/D.mp3");
+        soundD.play();
+    } else if(event.code === "KeyF"){
+        let soundF = new Audio("SoundKeys/F.mp3");
+        soundF.play();
+    } else if(event.code === "KeyG"){
+        let soundG = new Audio("SoundKeys/G.mp3");
+        soundG.play();
+    } else if(event.code === "KeyH"){
+        let soundH = new Audio("SoundKeys/H.mp3");
+        soundH.play();
+    } else if(event.code === "KeyJ"){
+        let soundJ = new Audio("SoundKeys/J.mp3");
+        soundJ.play();
+    } else if(event.code === "KeyW"){
+        let soundW = new Audio("SoundKeys/W.mp3");
+        soundW.play();
+    } else if(event.code === "KeyE"){
+        let soundE = new Audio("SoundKeys/E.mp3");
+        soundE.play();
+    } else if(event.code === "KeyT"){
+        let soundT = new Audio("SoundKeys/T.mp3");
+        soundT.play();
+    } else if(event.code === "KeyY"){
+        let soundY = new Audio("SoundKeys/Y.mp3");
+        soundY.play();
+    } else if(event.code === "KeyU"){
+        let soundU = new Audio("SoundKeys/U.mp3");
+        soundU.play();
+    }
+});
